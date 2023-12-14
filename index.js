4779618a92189c76f015414d00f0281f88b232b0
enzorodriguez-notbot <141461776+enzorodriguez-notbot@users.noreply.github.com>
2023-12-14 16:52:35 -0300

    a

---
+++ index.js
@@ -0,0 +1,7 @@
+let personas = [
+  { nombre: "Juan", edad: 25, ciudad: "Madrid" },
+  { nombre: "Ana", edad: 30, ciudad: "Barcelona" },
+  { nombre: "Carlos", edad: 22, ciudad: "Sevilla" },
+  { nombre: "María", edad: 28, ciudad: "Valencia" },
+  { nombre: "Pedro", edad: 35, ciudad: "Bilbao" },
+];
